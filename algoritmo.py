6e3f77c2685853042886ad84a38a69d0856d6d4b
Isaac A. Marin <VasMar_ia@hotmail.com>
2020-01-31 03:16:21 -0600

    Crossover Completed

--- algoritmo.py
+++ algoritmo.py
@@ -6,7 +6,7 @@ cant_genes_ind = 2
 cant_tipo_gen = 10
 tienda = {1:[7,3,10], 2:[6,3,8], 3:[4,5,3], 4:[10,5,4], 5:[12,10,2], 6:[3,8,6], 7:[5,8,1], 8:[8,9,2], 9:[9,12,0], 10:[12,12,2]}
 
-# poblacion inicial
+# Poblacion inicial
 population = {
   0:[[tienda[2],tienda[4]],0], 1:[[tienda[2],tienda[8]],0], 2:[[tienda[10],tienda[1]],0], 3:[[tienda[5],tienda[2]],0]
 , 4:[[tienda[10],tienda[6]],0], 5:[[tienda[9],tienda[5]],0], 6:[[tienda[2],tienda[3]],0], 7:[[tienda[6],tienda[3]],0], 8:[[tienda[1],tienda[5]],0]
@@ -18,17 +18,16 @@ population = {
 , 34:[[tienda[6],tienda[10]],0], 35:[[tienda[2],tienda[10]],0], 36:[[tienda[3],tienda[4]],0], 37:[[tienda[2],tienda[3]],0], 38:[[tienda[3],tienda[8]],0]
 , 39:[[tienda[5],tienda[2]],0] }
 
-# constantes para el calculos de fitness
+# Constantes para el calculos de fitness
 k1 = 1000
 k2 = 100
-# pesos
+# Pesos
 w_inventario = 0.5
 w_historia = 0.25
 w_distancia = 0.85
 
 
 def seleccion(generacion):
-    
     # alpha selection
     alpha = []
     alpha_data = 100
@@ -41,7 +40,6 @@ def seleccion(generacion):
             alpha_data = calificacion
             alpha = generacion[i]
             alpha_index = i
-    #print("best and minimum: ",alpha, "-", alpha_index)
     generacion.pop(alpha_index)
     
     # beta selection
@@ -93,11 +91,9 @@ def fitness(individuo):
     inventario_1 = individuo[0][0]
     distancia_1 = individuo[0][1]
     historial_1 = individuo[0][2]
-    #print("inventario: ", inventario_1, "distancia: ", distancia_1, "historial: ", historial_1)
     inventario_2 = individuo[1][0]
     distancia_2 = individuo[1][1]
     historial_2 = individuo[1][2]
-    #print("inventario: ", inventario_2, "distancia: ", distancia_2, "historial: ", historial_2)
     ct1 = (k1+ (inventario_1*w_inventario) - (distancia_1*w_distancia) - (historial_1*w_historia))/k2
     ct2 = (k1+ (inventario_2*w_inventario) - (distancia_2*w_distancia) - (historial_2*w_historia))/k2
     return(ct1+ct2)
@@ -108,17 +104,19 @@ def ajuste_pesos():
     w_historia+=(0.004)
     w_distancia+=(0.015)
 
-def mutacion():
+def mutacion(son1, son2):
     pass
 
 def crossover(alpha, beta):
     off_spring_2 = [beta[0],alpha[1]]
     off_spring_1 = [alpha[0],beta[1]]
-    print(off_spring_1)
-    print(off_spring_2)
+    mutacion(off_spring_1,off_spring_2)
+    return [off_spring_1,off_spring_2, alpha, beta]
 
 
 if __name__ == "__main__":
     the_best_gen = seleccion(population) # return [0] = alpha, [1] = beta
     print(the_best_gen)
-    crossover(the_best_gen[0][0],the_best_gen[1][0])
+    off_springs_parents = crossover(the_best_gen[0][0],the_best_gen[1][0])
+    print(off_springs_parents)
+    #generate_individuals()
