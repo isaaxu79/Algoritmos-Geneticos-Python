fd5a9462905c8b356991845264217092d7e47f09
Isaac A. Marin <VasMar_ia@hotmail.com>
2020-01-31 00:24:45 -0600

    Diccionario

--- test.py
+++ test.py
@@ -3,8 +3,13 @@ print(ga)
 for i in range(5):
     var = i+4
     print(var)
-    ga.update({var:[[1,1],0]})
+    if var == 6:
+        ga.update({var:[[0,2],0]})
+    else:
+        ga.update({var:[[1,1],0]})
 
 print(ga)
 ga.pop(7)
 print(ga)
+ex = ga.get(6)
+print(ex)
